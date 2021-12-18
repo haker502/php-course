5b6d3b914fcef189ca2285cad2b348e16ae7119e
haker502 <2776791998@qq.com>
2021-12-18 14:21:44 +0800

    add conditional statment

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
 	    echo "<h2>You have $".$credit." in your wallet.</h2>";
 
 		  $products['Computer']=750;
-	    $products['Car']=15000;
-	    $products['iPhone']=1000;
-	    $products['Toaster']=75;
+	          $products['Car']=15000;
+	          $products['iPhone']=1000;
+	          $products['Toaster']=75;
 
 	    foreach($products as $key => $value){
 		    echo "<p>The ".$key." costs ".$value."</p>";
@@ -22,11 +22,21 @@
 
 	    echo "<h2>Items you can afford</h2>";
 
-	    foreach($products as $key => $value){
+	    foreach($products as $key => $value)
+	    {
 		    if($value <= $credit ){
 		    	echo "<p>".$key."</p>"; 
 		    }
 	    }
+	    
+	    function tax_calc($amount,$tax)
+	    {
+  	    	$calculate_tax = $amount*$tax;
+  	    	$amount = round($amount+$calculate_tax,2);
+  	 	    return $amount;
+  	    }
+  	    
+  	    echo tax_calc(750,0.223);
     ?>
   </body>
 </html>
